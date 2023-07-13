1defe996a504c2ace1e5982a70906cf32aa4655c
Boris Drazhzhov <templar8@gmail.com>
2023-07-13 16:28:48 +0300

    Added icon displaying for Linux

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,15 +40,16 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ya_player");
+    gtk_header_bar_set_title(header_bar, "YaPlayer");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ya_player");
+    gtk_window_set_title(window, "YaPlayer");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
+  gtk_window_set_icon_from_file(GTK_WINDOW(window),"assets/app_icon.png",NULL);
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
