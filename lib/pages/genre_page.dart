067370b4086dd7cb0046fc855f239be326faaceb
Boris Drazhzhov <templar8@gmail.com>
2023-08-01 15:51:06 +0300

    Fixed broken genre page

--- lib/pages/genre_page.dart
+++ lib/pages/genre_page.dart
@@ -1,27 +1,60 @@
 import 'package:flutter/material.dart';
 
+import '../app_state.dart';
 import '../models/music_api/station.dart';
-import '../controls/page_base_layout.dart';
 import '../controls/station_genre.dart';
+import '../services/service_locator.dart';
 
 
 class GenrePage extends StatelessWidget {
   final Station genre;
+  final appState = getIt<AppState>();
 
-  const GenrePage({
+  static const double _minWidth = 250;
+  static const double _maxWidth = 412;
+
+  GenrePage({
     super.key, required this.genre,
   });
 
   @override
   Widget build(BuildContext context) {
-    return PageBaseLayout(
-      title: genre.name,
-      body: Wrap(children: genre.subStations.map((station) {
-        return Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: StationGenre(station)
-        );
-      }).toList()),
+    return SingleChildScrollView(
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          int columnsNumber = 3;
+          double width = constraints.maxWidth / columnsNumber;
+
+          while(true) {
+            if(width < _minWidth) columnsNumber -= 1;
+            if(columnsNumber == 0) {
+              columnsNumber = 1;
+              width = constraints.maxWidth;
+              break;
+            }
+            if(width > _maxWidth) columnsNumber += 1;
+
+            width = constraints.maxWidth / columnsNumber;
+
+            if(columnsNumber == 1 || width >= _minWidth && width <= _maxWidth) break;
+          }
+
+          return Wrap(children: genre.subStations.map((station) {
+            return Container(
+                constraints: BoxConstraints(maxWidth: width),
+                padding: const EdgeInsets.all(4.0),
+                child: GestureDetector(
+                    onTap: (){ appState.playStationTracks(station); },
+                    child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: StationGenre(station)
+                    )
+                )
+            );
+          }).toList()
+          );
+        },
+      ),
     );
   }
 }
