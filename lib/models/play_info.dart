3c220d33023d99fe7045477d983ab11d88bf4ca4
Boris Drazhzhov <templar8@gmail.com>
2024-11-04 20:34:17 +0300

    Improved PlayInfo class

--- lib/models/play_info.dart
+++ lib/models/play_info.dart
@@ -7,11 +7,10 @@ class PlayInfo {
   final Track track;
   final String from;
   var totalPlayed = const Duration();
-  late final String _uuid;
+  final String _uuid;
 
-  PlayInfo(this.track, this.from) {
-    _uuid = const Uuid().v4();
-  }
+  PlayInfo(this.track, this.from)
+      : _uuid = const Uuid().v4();
 
   Map<String, String> toYmPlayAudio() {
     final String dateTime = '${DateFormat('y-MM-ddTHH:mm:ss.S').format(DateTime.now())}Z';
@@ -26,8 +25,21 @@ class PlayInfo {
       'timestamp': dateTime,
       'client-now': dateTime,
       'track-length-seconds': (track.duration!.inMilliseconds / 1000.0).toString(),
+      // предполагаю, что тут должно находиться количество секунд, которые
+      // были проиграны. вот хороший пример:
+      // началось проигрываниве, трек проигрался 5 секнуд, пользователь
+      // решил пропустить/перемотать часть трека, к примеру 30 секунд,
+      // пользователь прослушал еще 30 секунд трека и решил перейти к следующем
+      // в этом случае 'total-played-seconds' = 35 секунд, а
+      // 'end-position-seconds' = 65 секунд
+      // значит 'total-played-seconds' — это фактическое время прослушивания трека,
+      // 'end-position-seconds' — это позиция, на которой прослушивание завершилось
       'total-played-seconds': totalPlayedSeconds,
       'end-position-seconds': totalPlayedSeconds
     };
+  }
+
+  bool isTheSameAs(Track track, String from) {
+    return from == this.from && track == this.track;
   }
 }
